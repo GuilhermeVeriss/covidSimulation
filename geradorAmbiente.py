fbdfc1d23b0cf36021ccea5116d23440bd6c84e2
Guilherme dos Santos Veríssimo <60589364+guilhermeveriss@users.noreply.github.com>
2020-10-24 12:43:49 -0300

    Interface gráfica
    Espaço para desenho e barra de opções

--- geradorAmbiente.py
+++ geradorAmbiente.py
@@ -19,18 +19,52 @@ def jpg_matrix(img):
     return list(map(lambda line: list(map(to_binary, line)), img))
 
 
-window = Tk()
-window.title("Gerador do Ambiente")
+root = Tk()
+root.title("Gerador do Ambiente")
+root.configure(background="white")
+
+# DESENHO
+drawSpace = Frame(root, padx=20, pady=20)
+drawSpace.pack(side=LEFT)
+
+# =========================================================================
+# OPÇÕES
+options = Frame(root, padx=20, pady=20)
+options.pack(side=RIGHT)
 
 imgTestjpg = Image.open("imagens/desenhoTestePlanta.jpg").convert('L')
 testMatrix = jpg_matrix(imgTestjpg)
 
+startSelection = Button(options, width=40, text="Iniciar seleção")
+startSelection.grid(row=1, column=1)
+
+
+# FERRAMENTAS DE SELEÇÃO
+# Container
+selectionTools = Frame(options)
+selectionTools.grid(row=2, column=1)
+
+# Selecionar
+select = Button(selectionTools, width=10, text="Selecionar")
+select.pack(side=RIGHT)
+# Apagar
+erase = Button(selectionTools, width=10, text="Apagar")
+erase.pack(side=LEFT)
+
+# -----------------------------------------------------------------------
+
+
+# GERA O DESENHO
 for r in range(len(testMatrix)):
     for c in range(len(testMatrix[r])):
-        lbNumber = Button(window, width=0, height=1, bd=0, font="Arial, 5", text=str(testMatrix[r][c]))
-        if testMatrix[r][c] == 0:
-            lbNumber["bg"] = "black"
-        lbNumber.grid(row=r, column=c)
-        # print(r, c)
 
-window.mainloop()
+        if testMatrix[r][c] == 0:
+            lbNumber = Label(drawSpace, width=0, height=1, bd=0, font="Arial, 5", text=str(testMatrix[r][c]))
+            lbNumber.grid(row=r, column=c)
+            lbNumber["bg"] = "black"
+        else:
+            btNumber = Button(drawSpace, width=0, height=1, bd=0, font="Arial, 5", text=str(testMatrix[r][c]))
+            btNumber.grid(row=r, column=c)
+
+
+root.mainloop()

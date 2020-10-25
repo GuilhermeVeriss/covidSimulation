b7ee296eee71b07467dc47494297bb0ad2dec1f6
Guilherme Verissimo <guilherme.santos.verissimo@gmail.com>
2020-10-24 21:31:53 -0300

    Interface gráfica
    Bug: ao apertar botão do pixel, em vez de o comando passar como parâmetro as coordenadas do próprio pixel, ele passa as coordenadas do último.

--- geradorAmbiente.py
+++ geradorAmbiente.py
@@ -2,6 +2,7 @@ from tkinter import *
 from PIL import Image
 from numpy import array
 from numpy.linalg import norm
+from copy import deepcopy
 
 
 def jpg_matrix(img):
@@ -17,6 +18,14 @@ def jpg_matrix(img):
 
     img = list(array(img))
     return list(map(lambda line: list(map(to_binary, line)), img))
+
+
+lis = []
+
+
+def select(a, b):
+    # lis.append(pos)
+    print((a, b))
 
 
 root = Tk()
@@ -39,32 +48,40 @@ startSelection = Button(options, width=40, text="Iniciar seleção")
 startSelection.grid(row=1, column=1)
 
 
+finishSelection = Button(options, width=40, text="Concluir seleção")
+finishSelection.grid(row=3, column=1)
+
 # FERRAMENTAS DE SELEÇÃO
 # Container
 selectionTools = Frame(options)
 selectionTools.grid(row=2, column=1)
 
 # Selecionar
-select = Button(selectionTools, width=10, text="Selecionar")
-select.pack(side=RIGHT)
+btSelect = Button(selectionTools, width=10, text="Selecionar")
+btSelect.pack(side=RIGHT)
 # Apagar
-erase = Button(selectionTools, width=10, text="Apagar")
-erase.pack(side=LEFT)
+btErase = Button(selectionTools, width=10, text="Apagar")
+btErase.pack(side=LEFT)
 
 # -----------------------------------------------------------------------
+
+pixels = deepcopy(testMatrix)
 
 
 # GERA O DESENHO
 for r in range(len(testMatrix)):
     for c in range(len(testMatrix[r])):
-
         if testMatrix[r][c] == 0:
             lbNumber = Label(drawSpace, width=0, height=1, bd=0, font="Arial, 5", text=str(testMatrix[r][c]))
-            lbNumber.grid(row=r, column=c)
+            lbNumber.grid(row=r+1, column=c+1)
             lbNumber["bg"] = "black"
         else:
-            btNumber = Button(drawSpace, width=0, height=1, bd=0, font="Arial, 5", text=str(testMatrix[r][c]))
-            btNumber.grid(row=r, column=c)
+            pixels[r][c] = Button(drawSpace, width=0, height=1, bd=0, font="Arial, 5", text=str(testMatrix[r][c]))
+            pixels[r][c].grid(row=r+1, column=c+1)
+            # BUG
+            pixels[r][c].configure(command=lambda: select()) # BUG
 
 
 root.mainloop()
+
+

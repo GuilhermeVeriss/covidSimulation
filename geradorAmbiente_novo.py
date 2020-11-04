2b32d4fee7fe2e50c78dca2d164321ebd8ee6f73
Guilherme Verissimo <guilherme.santos.verissimo@gmail.com>
2020-11-04 10:08:20 -0300

    Interface Gráfica
    Seleção por cor concluída. Já é possível gera o ambiente

--- geradorAmbiente_novo.py
+++ geradorAmbiente_novo.py
@@ -1,19 +1,40 @@
 from tkinter import *
+from tkinter import simpledialog
 from numpy import array
 from PIL import Image
 from copy import deepcopy
+from pickle import dump
 
 
 def rgb_color(rgb):
     return '#%02x%02x%02x' % rgb
 
 
+def input_colors():
+    global regions
+    for hex_color in color_code:
+        color_show.configure(bg=hex_color)
+        while True:
+            name = simpledialog.askstring("Inserir nome", "Insira o nome da região")
+            if name:
+                regions[name] = color_code[hex_color]
+                break
+
+
 imgName = "imagens/color_identifier.png"
+save_data = "ambiente.xml"
+
+
 imgTestJpg = Image.open(imgName)
 
 imgArray = array(imgTestJpg)
 
-matrix_res = deepcopy(imgArray)
+matrix_res = list(deepcopy(imgArray))
+matrix_res = list(map(lambda x: list(x), matrix_res))
+
+color_code = {}
+regions = {}
+
 
 root = Tk()
 root.title("Selecionar Regiões")
@@ -27,22 +48,26 @@ options.pack(side=RIGHT)
 color_show = Label(options, width=15, height=5)
 color_show.pack(side=TOP)
 
-select = Button(options, width=30, text="Definir nome")
+select = Button(options, width=30, text="Definir nome", command=input_colors)
 select.pack(side=BOTTOM)
 
 canvas = Canvas(draw_space, width=len(imgArray[0]), height=len(imgArray))
 canvas.pack()
 
 
-color_code = {}
-
 for r in range(len(imgArray)):
     for c in range(len(imgArray[r])):
         color = rgb_color(tuple(imgArray[r][c]))
         if color not in color_code.keys():
             color_code[color] = len(color_code) + 1
+        matrix_res[r][c] = color_code[color]
         canvas.create_rectangle(r, c, r+1, c+1, fill=color, width=0)
 
-print(color_code)
 
 root.mainloop()
+
+env = {"regions": regions, "matrix": matrix_res}
+
+fontText = open(save_data, "wb")
+dump(env, fontText)
+fontText.close()
--- simulador.py
+++ simulador.py
@@ -6,10 +6,14 @@ from PIL import Image, ImageTk
 font = open("ambiente.xml", "rb")
 regions, matrixImg = load(font).values()
 
-root = Tk()
+print(regions)
+for i in matrixImg:
+    print(i)
 
-img = ImageTk.PhotoImage(Image.open("imagens/desenhoTestePlanta.jpg"))
-canvas = Canvas(root, width=img.width(), height=img.height())
-canvas.create_image(0, 0, anchor=NW, image=img)
-canvas.pack()
-root.mainloop()
+# root = Tk()
+
+# img = ImageTk.PhotoImage(Image.open("imagens/desenhoTestePlanta.jpg"))
+# canvas = Canvas(root, width=img.width(), height=img.height())
+# canvas.create_image(0, 0, anchor=NW, image=img)
+# canvas.pack()
+# root.mainloop()

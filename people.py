0d627846d133181d4102bd3154ff638855503d62
Guilherme Verissimo <guilherme.santos.verissimo@gmail.com>
2020-11-05 12:23:15 -0300

    Simulador
    Função create_person

--- people.py
+++ people.py
@@ -2,13 +2,18 @@ from data import *
 from numpy import array
 
 
-def create_person():
+def create_person(pos):
 
-    return {
-        "pos": [],
+    person = {
+        "pos": array([pos]),
         "vel": [],
-        "size": [],
+        "size": 3,
         "job": [],       # Armazena as informações da pessoa, como rotina, movimentação, etc
         "visionfield": [],
         "route": []
     }
+    person["creationArguments"] = [pos[0]-person["size"], pos[1]-person["size"],
+                                   pos[0]+person["size"], pos[1]+person["size"]]
+
+    return person
+
--- simulador.py
+++ simulador.py
@@ -1,5 +1,6 @@
 from pickle import load
 from tkinter import *
+from people import *
 
 
 font = open("ambiente.xml", "rb")
@@ -13,6 +14,8 @@ root.title("Ambiente da simulação")
 
 canvas = Canvas(root, bg="white", width=matrix_w, height=matrix_h)
 canvas.pack()
+
+canvas.create_oval(*create_person((20, 20))["creationArguments"], fill="blue")
 
 for r in range(matrix_h):
     for c in range(matrix_w):
